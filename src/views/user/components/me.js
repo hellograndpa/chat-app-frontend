9121f7dfb9808fd324baf75580b3266eb8e53465
Antonio Rivera <antonioriver@gmail.com>
2019-11-02 09:57:26 +0100

    user wip

---
+++ src/views/user/components/me.js
@@ -0,0 +1,8 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+
+export default class me extends Component {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/views/user/meUser.js
@@ -0,0 +1,14 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+
+export default class meUser extends Component {
+  render() {
+    return (
+      <div>
+        <div>misn chat privados</div>
+        <div>mi usuario</div>
+        <div>mis rooms</div>
+      </div>
+    );
+  }
+}
