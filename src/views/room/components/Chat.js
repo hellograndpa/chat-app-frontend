8ed5d9396c676296279a4e2a83852a7dfa41952a
Victor Camarena Ballester <victor.camarena.ballester@gmail.com>
2019-11-08 03:21:20 +0100

    form rooms

--- src/views/room/components/Chat.js
+++ src/views/room/components/Chat.js
@@ -1,61 +1,56 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import socketIOClient from 'socket.io-client';
-import RoomService from '../../../services/roomService';
+
+// Services
 import ChatRoomService from '../../../services/chatRoomService';
+
+// Css
 import '../../../index.css';
 
 const socket = socketIOClient('localhost:3001');
 
 class Chat extends Component {
   state = {
-    roomId: 0,
-    chatId: 0,
-    conversation: [],
-    loading: true,
+    room: this.props.room,
   };
 
   async componentDidMount() {
-    // Recevie RoomId from parent
-    const { roomId } = this.props;
-
-    // Look for room available
-    const room = await RoomService.getRoomById(roomId);
-
-    // Fill the state with initial conversation
-    this.setState({
-      roomId: room._id,
-      chatId: room.chat._id,
-      conversation: room.chat.conversation,
-      loading: false,
-    });
+    const {
+      chat: { _id: chatId },
+    } = this.state.room;
 
     // Listen for changes on the db.
-
-    socket.on(room.chat._id, message => {
-      const newConversation = [...this.state.conversation, message];
-      this.setState({ conversation: newConversation });
+    socket.on(chatId, message => {
+      const newConversation = [...this.state.room.chat.conversation, message];
+      this.setState({ room: { chat: { conversation: newConversation } } });
     });
   }
 
   handleSubmit = async e => {
     e.preventDefault();
     // Get the chat Id
-    const { chatId } = this.state;
+    const {
+      chat: { _id: chatId },
+    } = this.state.room;
+
     const text = e.target.text.value;
 
     // Update the db with the new comment,
     // This will fire a emit on server, we will catch it with the
     // socket.on above
-    await ChatRoomService.updateChatUser(chatId, text);
+    // asyncrosous, without wait
+    ChatRoomService.updateChatUser(chatId, text);
 
     // Clean the input
     this.input.value = '';
   };
 
   async componentCleanup() {
-    const { chatId } = this.state;
-    socket.removeAllListeners(`chat${  chatId}`);
+    const {
+      chat: { _id: chatId },
+    } = this.state.room;
+    socket.removeAllListeners(`chat${chatId}`);
   }
 
   componentWillUnmount() {
@@ -63,21 +58,21 @@ class Chat extends Component {
   }
 
   render() {
-    const { conversation, loading } = this.state;
+    const {
+      chat: { conversation },
+    } = this.state.room;
 
     return (
       <>
         <div className="chatwrapper">
           <div className="chat">
-            {!loading &&
-              conversation.map((c, index) => {
-                return (
-                  <div key={index}>
-                    {c.user.userName}: {c.text}
-                  </div>
-                );
-              })}
-            {loading && 'loading...'}
+            {conversation.map((c, index) => {
+              return (
+                <div key={index}>
+                  {c.user.userName}: {c.text}
+                </div>
+              );
+            })}
             <div className="anchor"></div>
           </div>
         </div>
---
+++ src/views/room/components/RoomDetails.js
@@ -0,0 +1,143 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+
+// Services
+import RoomService from '../../../services/roomService';
+
+// Components
+import FileUpload from '../../../components/FileUpload';
+
+class RoomsDetails extends Component {
+  state = {
+    ...this.props,
+    editMode: false,
+  };
+
+  handleEditMode = () => {
+    const { editMode } = this.state;
+    this.setState({ editMode: !editMode });
+  };
+
+  handleOnChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleChangeTheme = e => {
+    this.setState({
+      theme: e.target.value,
+    });
+  };
+
+  handleOnSubmit = async e => {
+    e.preventDefault();
+    const { _id, roomName, description, adminList, userBanList, avatar, city, theme } = this.state;
+    await RoomService.updateRoom({
+      _id,
+      roomName,
+      description,
+      adminList,
+      userBanList,
+      avatar,
+      city,
+      theme,
+    });
+    this.setState({ editMode: false });
+  };
+
+  render() {
+    const { roomName, description, adminList, userBanList, avatar, city, theme } = this.state;
+    const { editMode } = this.state;
+    return (
+      <div>
+        {editMode && (
+          <div>
+            <form name="frm" onSubmit={this.handleOnSubmit}>
+              <button onClick={this.handleEditMode}>Edit mode off</button>
+              <div>
+                Room Name: <input type="text" name="roomName" value={roomName} onChange={this.handleOnChange} />
+              </div>
+              <div>
+                Description: <input type="text" name="description" value={description} onChange={this.handleOnChange} />
+              </div>
+              <div>
+                AdminList:
+                <br />
+                <ul>
+                  {adminList.map(user => {
+                    return (
+                      <li key={user._id}>
+                        {user.userName} <button>quitar</button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div>
+                userBanList:
+                <br />
+                <ul>
+                  {userBanList.map(user => {
+                    return (
+                      <li key={user._id}>
+                        {user.userName}
+                        <button>quitar</button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <div>
+                avatar:
+                <FileUpload></FileUpload>
+              </div>
+              <div>
+                City: <input type="text" name="city" onChange={this.handleOnChange} value={city} />
+              </div>
+              <div>
+                Theme:
+                <select name="theme" defaultValue={this.state.theme} onChange={this.handleChangeTheme}>
+                  <option value="Travel">Travel</option>
+                  <option value="Party">Party</option>
+                  <option value="News">News</option>
+                  <option value="Shopping">Shopping</option>
+                  <option value="Pets">Pets</option>
+                </select>
+              </div>
+              <input type="submit" value="Send"></input>
+            </form>
+          </div>
+        )}
+        {!editMode && (
+          <div>
+            <button onClick={this.handleEditMode}>Edit mode on</button>
+            <div>Room Name: {roomName}</div>
+            <div>Description: {description}</div>
+            <div>
+              AdminList:
+              <br />
+              <ul>
+                {adminList.map(user => {
+                  return <li key={user._id}>{user.userName}</li>;
+                })}
+              </ul>
+            </div>
+            <div>
+              userBanList:
+              <br />
+              <ul>
+                {userBanList.map(user => {
+                  return <li key={user._id}>{user.userName}</li>;
+                })}
+              </ul>
+            </div>
+            <div>avatar: {avatar}</div>
+            <div>City:{city}</div>
+            <div>Theme:{theme}</div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default RoomsDetails;
