24f4426235f022a68b4826559012fa0a49a175db
Antonio Rivera <antonioriver@gmail.com>
2019-11-09 03:18:53 +0100

    nav

--- src/views/nav/NavBar.js
+++ src/views/nav/NavBar.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+// images
+import icons_comment from '../../images/icons/icons_comment.svg';
+import icons_rideshare from '../../images/icons/icons_rideshare.svg';
+import icons_users from '../../images/icons/icons_users.svg';
+
 const NavBar = () => {
   return (
-    <>
+    <div>
       <div>
         <div>izq</div>
         <div>der</div>
       </div>
-      <div>
-        <span>
-          <NavLink to="/">You</NavLink>
-        </span>
-        <span>
-          <NavLink to="/rooms/list">Rooms Tour</NavLink>
-        </span>
-        <span>
-          <NavLink to="/users/list">Users Tour</NavLink>
-        </span>
+      <div className="menu-principal ripple">
+        <di className="element01 ripple">
+          <div>
+            <img className="icon" src={icons_rideshare} />
+          </div>
+          <NavLink className="nav" to="/">
+            You
+          </NavLink>
+        </di>
+        <div className="element02 ripple">
+          <div>
+            <img className="icon" src={icons_comment} />
+          </div>
+          <NavLink className="nav" to="/rooms/list">
+            Rooms Tour
+          </NavLink>
+        </div>
+        <div className="element03 ripple">
+          <div>
+            <img className="icon" src={icons_users} />
+          </div>
+          <NavLink className="nav" to="/users/list">
+            Users Tour
+          </NavLink>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
