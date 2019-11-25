f3b7c6417e09540b2951bfb19f62bc81d58f6958
Victor Camarena Ballester <victor.camarena.ballester@gmail.com>
2019-11-25 18:11:46 +0100

    fixing voice

--- src/helpers/voicer.js
+++ src/helpers/voicer.js
@@ -1,21 +1,23 @@
 export default cb => {
   window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-  let finalTranscript = '';
-  const recognition = new window.SpeechRecognition();
-  recognition.interimResults = false;
-  recognition.maxAlternatives = 10;
-  recognition.continuous = false;
-  recognition.onresult = event => {
-    // let interimTranscript = '';
-    for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
-      const { transcript } = event.results[i][0];
-      if (event.results[i].isFinal) {
-        finalTranscript += transcript;
-      } else {
-        // interimTranscript += transcript;
+  if (window.SpeechRecognition) {
+    let finalTranscript = '';
+    const recognition = new window.SpeechRecognition();
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 10;
+    recognition.continuous = false;
+    recognition.onresult = event => {
+      // let interimTranscript = '';
+      for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+        const { transcript } = event.results[i][0];
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript;
+        } else {
+          // interimTranscript += transcript;
+        }
       }
-    }
-    cb(finalTranscript);
-  };
-  recognition.start();
+      cb(finalTranscript);
+    };
+    recognition.start();
+  }
 };
